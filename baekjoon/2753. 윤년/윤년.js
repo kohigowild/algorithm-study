73c3dc4ffe738bb5ff2b5de12e823f407c881f79
kohi <mainmirror14@gmail.com>
2024-07-21 14:45:35 +0900

    2753. 윤년

---
+++ baekjoon/2753. 윤년/윤년.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString()
+let year = parseInt(input)
+
+if (year % 4 === 0 && year % 100 !== 0) {
+  console.log(1)
+} else if (year % 400 === 0) {
+  console.log(1)
+} else {
+  console.log(0)
+}
